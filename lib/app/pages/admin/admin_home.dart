283ad8e61593f11dd3e6e841d87d7b6148a28da6
Andrei Sas <andreisas007@gmail.com>
2022-12-06 19:06:00 +0100

    created product list tile widget

--- lib/app/pages/admin/admin_home.dart
+++ lib/app/pages/admin/admin_home.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerce_firebase/app/pages/admin/admin_add_product.dart';
 import 'package:ecommerce_firebase/app/providers.dart';
 import 'package:ecommerce_firebase/models/product.dart';
+import 'package:ecommerce_firebase/widgets/product_list_tile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 
 class AdminHome extends ConsumerWidget {
   const AdminHome({super.key});
@@ -28,24 +30,33 @@ class AdminHome extends ConsumerWidget {
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.active &&
               snapshot.data != null) {
+            if (snapshot.data!.isEmpty) {
+              return Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text("No products yet..."),
+                    Lottie.asset("assets/anim/empty.json", // here
+                        width: 200,
+                        repeat: true),
+                  ],
+                ),
+              );
+            }
             return ListView.builder(
                 itemCount: snapshot.data!.length,
                 itemBuilder: ((context, index) {
                   final product = snapshot.data![index];
-                  return ListTile(
-                    title: Text(product.name),
-                    subtitle: Text("Price: " + product.price.toString()),
-                    leading: product.imageUrl != null
-                        ? Image.network(
-                            product.imageUrl,
-                          )
-                        : Container(),
-                    trailing: IconButton(
-                      onPressed: (() => ref
+                  return ProductListTile(
+                    product: product,
+                    onDelete: (() async {
+                      await ref
                           .read(databaseProvider)!
-                          .deleteProduct(product.id!)),
-                      icon: const Icon(Icons.delete),
-                    ),
+                          .deleteProduct(product.id!);
+                    }),
+                    onPressed: () {
+                      print("test");
+                    },
                   );
                 }));
           }
